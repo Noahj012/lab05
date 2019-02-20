7bfa11e8d251b1d36ab48eff04514a1b73a7f4b9
Noah012 <noahpierce185016@gmail.com>
2019-02-19 20:02:21 -0600

    finished todo list

--- Todo.java
+++ Todo.java
@@ -1,18 +1,27 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 12
+Last Modified_______: 2019-16-19
+OU ID_______________: 113484432
+OU EMAIL____________: noah.j.pierce@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. calculate two tokens method in the calculator class
+2. calculate three tokens method in the calculator class
+3. execute method in the calculator class
+4. parseandexecute method in the calculator class
+5. do method test in the calculatorTest class
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 10
+3. 15
+4. 10
+5. 30
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 12
+2. 16
+3. 25
+4. 11
+5. 66
 */}
